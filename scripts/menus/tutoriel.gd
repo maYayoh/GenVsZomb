1c18968bf5ef41db429721578c0a0b89c9850583
Corentin Boblet <corentinboblet@gmail.com>
2023-08-15 14:00:37 +0200

    Tutorial

--- scripts/menus/tutoriel.gd
+++ scripts/menus/tutoriel.gd
@@ -1,22 +1,28 @@
 extends Node2D
 
-@onready var sprite : Sprite2D = $Sprite2D
+@onready var text : Sprite2D = $Sprite2D
+@onready var sprites : Sprite2D = $Sprite2D2
 
 func _ready():
 	position.y = 64
 	get_node("/root/main_menu/Camera2D").offset.y = 64
+	
+	text.frame = 0
+	sprites.frame = 0
 
 func _on_back_pressed():
-	if(sprite.frame == 0):
+	if(text.frame == 0):
 		get_node("/root/main_menu/Camera2D").offset.y = 0
 		queue_free()
 	else:
-		sprite.frame -= 1
+		text.frame -= 1
+		sprites.frame -= 1
 
 
 func _on_next_pressed():
-	if(sprite.frame == 5):
+	if(text.frame == 5):
 		get_node("/root/main_menu/Camera2D").offset.y = 0
 		queue_free()
 	else:
-		sprite.frame += 1
+		text.frame += 1
+		sprites.frame += 1
